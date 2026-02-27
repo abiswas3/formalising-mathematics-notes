fa20abdd090d843fcdb999fe49ac7fd46f6b7a5d
Ari <software@randomwalks.xyz>
2026-02-27 00:35:11 +0000

    more sheets

--- FormalisingMathematics2026/Section04sets/Sheet5.lean
+++ FormalisingMathematics2026/Section04sets/Sheet5.lean
@@ -35,22 +35,113 @@ variable (X : Type)
   (x y z : X)
 
 -- x,y,z are elements of `X` or, more precisely, terms of type `X`
-example : A ∪ A = A := by sorry
+example : A ∪ A = A := by
+  ext x 
+  constructor 
+  . intro h 
+    rcases h with l | r 
+    . assumption
+    . assumption
+  . intro h 
+    left 
+    exact h 
 
-example : A ∩ A = A := by sorry
 
-example : A ∩ ∅ = ∅ := by sorry
+example : A ∩ A = A := by 
+  ext a
+  constructor 
+  . intro ha 
+    exact ha.left 
+  . intro ha 
+    rw [mem_inter_iff] 
+    constructor
+    . exact ha 
+    . exact ha 
 
-example : A ∪ univ = univ := by sorry
 
-example : A ⊆ B → B ⊆ A → A = B := by sorry
 
-example : A ∩ B = B ∩ A := by sorry
+example : A ∩ ∅ = ∅ := by 
+  ext a 
+  constructor
+  . intro ha 
+    rw [mem_inter_iff] at ha 
+    exact ha.right
+  . intro ha 
+    rw [mem_inter_iff] 
+    constructor 
+    . by_contra hb 
+      apply ha
+    . assumption
 
-example : A ∩ (B ∩ C) = A ∩ B ∩ C := by sorry
+example : A ∪ univ = univ := by 
+  ext x 
+  constructor 
+  . intro h
+    rcases h with h | h
+    . trivial  
+    . exact h 
+  . intro h 
+    right; exact h 
 
-example : A ∪ (B ∪ C) = A ∪ B ∪ C := by sorry
+example : A ⊆ B → B ⊆ A → A = B := by 
+  intro h h1
+  ext x 
+  constructor 
+  . intro ha 
+    rw [subset_def] at h
+    specialize h x ha 
+    exact h
+  . intro hb 
+    rw [subset_def] at h1
+    specialize h1 x hb
+    exact h1
 
-example : A ∪ B ∩ C = (A ∪ B) ∩ (A ∪ C) := by sorry
+-- TODO: I'll do this later.
 
-example : A ∩ (B ∪ C) = A ∩ B ∪ A ∩ C := by sorry
+example : A ∩ B = B ∩ A := by 
+  ext x 
+  constructor 
+  . intro h 
+    constructor 
+    . exact h.right 
+    . exact h.left
+  . intro h 
+    constructor 
+    . exact h.right 
+    . exact h.left 
+
+example : A ∩ (B ∩ C) = A ∩ B ∩ C := by 
+  ext x 
+  constructor 
+  . intro h 
+    have h1: x ∈ A := h.left 
+    have h2: x ∈ B := (h.right).left 
+    have h3: x ∈ C := (h.right).right 
+    constructor 
+    . constructor <;> assumption
+    . exact h3
+  . intro h 
+    have h1: x ∈ C := h.right 
+    have h2: x ∈ B := h.left.right
+    constructor 
+    . exact (h.left).left  
+    . constructor <;> assumption
+
+example : A ∪ (B ∪ C) = A ∪ B ∪ C := by 
+  ext x 
+  constructor 
+  . intro h 
+    rcases h with h' | h' | h'
+    . left; left; assumption 
+    . left; right; assumption 
+    . right; assumption 
+  . intro h 
+    rcases h with h' | h' 
+    . rcases h' with h'' | h''
+      . left; assumption
+      . right; left; assumption 
+    . right; right; assumption 
+
+-- TODO: I am really really bored -- aesop is super smart simp 
+example : A ∪ B ∩ C = (A ∪ B) ∩ (A ∪ C) := by aesop
+example : A ∩ (B ∪ C) = A ∩ B ∪ A ∩ C := by aesop
--- FormalisingMathematics2026/Section04sets/Sheet6.lean
+++ FormalisingMathematics2026/Section04sets/Sheet6.lean
@@ -38,25 +38,69 @@ for group theory. In Lean we use the notation `f ⁻¹' T` for this pullback.
 
 variable (X Y : Type) (f : X → Y) (S : Set X) (T : Set Y)
 
-example : S ⊆ f ⁻¹' (f '' S) := by sorry
+example : S ⊆ f ⁻¹' (f '' S) := by 
+  intro a ha
+  rw [Set.mem_preimage, Set.mem_image]
+  -- |⊢ ∃ x ∈ S, f x = f a
+  use a  -- why did this work
 
-example : f '' (f ⁻¹' T) ⊆ T := by sorry
+
+example : f '' (f ⁻¹' T) ⊆ T := by 
+ intro y hy
+ -- rcases hy with ⟨x, hx⟩ -- hx : x ∈ f ⁻¹' T ∧ (f x = y)  and the goal |- y ∈ T 
+ rcases hy with ⟨x, hx, rfl⟩ -- hx : x ∈ f ⁻¹' T  and goal |- f x ∈ T (using the rfl) 
+ rw [Set.mem_preimage] at hx 
+ exact hx
 
 -- `exact?` will do this but see if you can do it yourself.
-example : f '' S ⊆ T ↔ S ⊆ f ⁻¹' T := by sorry
+example : f '' S ⊆ T ↔ S ⊆ f ⁻¹' T := by 
+  constructor 
+  . intro h s sx  
+    rw [Set.subset_def] at *
+    rw [Set.mem_preimage] 
+    apply h
+    rw [Set.mem_image]
+    use s 
+  . intro h y hy 
+    rw [Set.subset_def] at *
+    rw [Set.mem_image] at * 
+    rcases hy with ⟨ a, b, rfl⟩
+    specialize h a b
+    rw [Set.mem_preimage] at * 
+    exact h
 
 -- Pushforward and pullback along the identity map don't change anything
 -- pullback is not so hard
-example : id ⁻¹' S = S := by sorry
+example : id ⁻¹' S = S := by 
+  ext x 
+  constructor 
+  . intro h 
+    rw[Set.mem_preimage] at h 
+    exact h
+  . intro h
+    rw[Set.mem_preimage] at *
+    exact h 
 
 -- pushforward is a little trickier. You might have to `ext x`, `constructor`.
-example : id '' S = S := by sorry
-
+example : id '' S = S := by 
+  ext x 
+  constructor
+  . intro h 
+    rw [Set.mem_image] at h 
+    obtain ⟨a , h1, rfl⟩ := h
+    exact h1 
+  . intro h
+    rw [Set.mem_image] 
+    use x
+    constructor
+    . exact h 
+    . rfl 
+    
 -- Now let's try composition.
 variable (Z : Type) (g : Y → Z) (U : Set Z)
 
+-- TODO: I get so bored by the end -- i should stop now 27/2/2026
 -- preimage of preimage is preimage of comp
-example : g ∘ f ⁻¹' U = f ⁻¹' (g ⁻¹' U) := by sorry
-
+example : g ∘ f ⁻¹' U = f ⁻¹' (g ⁻¹' U) := by aesop 
 -- preimage of preimage is preimage of comp
-example : g ∘ f '' S = g '' (f '' S) := by sorry
+example : g ∘ f '' S = g '' (f '' S) := by aesop 
